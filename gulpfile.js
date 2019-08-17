46f9e146f5ca754a1291b82248248000ae5da136
stephendoyin <stevedoyin@gmail.com>
2019-08-17 01:09:36 +0100

    added build for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,12 @@ require('./gulp/tasks/styles');
 require('./gulp/tasks/modernizr');
 require('./gulp/tasks/scripts');
 require('./gulp/tasks/watch');
+require('./gulp/tasks/build');
 
 
 exports.watch = series('watch', 
   series(series('styles', 'cssInject'), series(series('modernizr', 'scripts'), 'scriptsRefresh'))
 );
+
+
+exports.build = series('deleteDistFolder', 'copyGeneralFiles', series('styles', 'scripts', 'usemin'))
